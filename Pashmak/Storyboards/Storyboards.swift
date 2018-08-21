ff8f2c06260403252436dc4b0267ee0b4e30df9d
Mohammad Porooshani <porooshani@gmail.com>
2018-08-21 10:11:34 +0430

    Added Scenes and storyboards
    Signed-off-by: Mohammad Porooshani <porooshani@gmail.com>

---
+++ Pashmak/Storyboards/Storyboards.swift
@@ -0,0 +1,16 @@
+//
+//  Storyboards.swift
+//  Pashmak
+//
+//  Created by Mohammad Porooshani on 8/21/18.
+//  Copyright © 2018 Mohammad Porooshani. All rights reserved.
+//
+
+import UIKit
+
+struct Storyboards {
+  
+  
+  
+  
+}
